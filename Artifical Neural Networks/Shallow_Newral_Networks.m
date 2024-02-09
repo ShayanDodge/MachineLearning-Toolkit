be7bb00ea4a6093ac26b27dae474eeb32b7868e8
shayan <dodgeshayan@gmail.com>
2024-02-09 12:10:20 +0100

    1

--- Artifical Neural Networks/Shallow_Newral_Networks.m
+++ Artifical Neural Networks/Shallow_Newral_Networks.m
@@ -1,4 +1,7 @@
 function Shallow_Newral_Networks
     load bodyfat_dataset.mat
-    net = feedforeard;
-    net = configure(net, bodyfatInputs, bodyfatTargets)
+    net = feedforwardnet(20);
+    net = configure(net, bodyfatInputs, bodyfatTargets);
+    net = init(net);
+    [net,tr] = train(net,bodyfatInputs,bodyfatTargets);
+    net.trainParam
