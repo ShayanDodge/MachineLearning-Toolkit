990c96ce6f3c621987805f5fc6d5b71671ea1a84
shayan <dodgeshayan@gmail.com>
2024-02-17 21:02:31 +0100

    dataset

---
+++ End_to_End Project/dataset_loader.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import sys
+import os
+
+# Function to find the directory of a file
+def find_directory(filename=None):
+    if filename is None:
+        filename = sys.argv[1]
+    current_directory = os.getcwd()
+    file_path = os.path.join(current_directory, filename)
+    return file_path
+
+# Function to load the dataset from a CSV file
+def load_data(csv_path):
+    return pd.read_csv(csv_path)
+
+# Load the dataset using the default path
+if __name__ == "__main__":
+    df = load_data(find_directory("datasets\housing\housing.csv"))
+    print(df.head())
