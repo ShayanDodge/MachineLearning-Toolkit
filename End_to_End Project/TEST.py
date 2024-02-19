5ef2afdddc0d3d370c0ad107c93a2f8f25786e97
shayan <dodgeshayan@gmail.com>
2024-02-19 07:46:40 +0100

    first

--- End_to_End Project/TEST.py
+++ End_to_End Project/TEST.py
@@ -1,3 +1,3 @@
-data = "example"
+data = '123'
 hash_code = hash(data)
 print(hash_code)
--- End_to_End Project/dataset_spliter.py
+++ End_to_End Project/dataset_spliter.py
@@ -4,7 +4,6 @@ from zlib import crc32
 # to make this script's output identical at every run
 np.random.seed(42)
 
-# For illustration only. Sklearn has train_test_split()
 def split_train_test(data, test_ratio):
     shuffled_indices = np.random.permutation(len(data))
     test_set_size = int(len(data) * test_ratio)
@@ -20,25 +19,23 @@ def test_set_check(identifier, test_ratio):
 def split_train_test_by_id(data, test_ratio, id_column):
     # Extract identifiers from the specified column in the dataset
     ids = data[id_column]
-    
     # Apply the test_set_check function to each identifier
-    # to determine whether the instance belongs to the test set
     in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
-    
     # Return the training set (not in the test set) and the test set
     return data.loc[~in_test_set], data.loc[in_test_set]
 
 if __name__ == "__main__":
     import dataset_loader
     df = dataset_loader.load_data(dataset_loader.find_directory("datasets\housing\housing.csv"))
-    train_set, test_set = split_train_test(df, 0.2)
-    print('The number of train set is: ', len(train_set))
-    print('The number of train set is: ', len(test_set))
 
-    housing_with_id = housing.reset_index() # adds an `index` column
-    train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "index")
+    # train_set, test_set = split_train_test(df, 0.2)
+    # print('The number of train set is: ', len(train_set))
+    # print('The number of train set is: ', len(test_set))
 
-    housing_with_id["id"] = housing["longitude"] * 1000 + housing["latitude"]
-    train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "id")
+    df_with_id = df.reset_index() # adds an `index` column#
+    # train_set, test_set = split_train_test_by_id(df_with_id, 0.2, "index")
 
-    test_set.head()
+    df_with_id["id"] = df["longitude"] * 1000 + df["latitude"]
+    train_set, test_set = split_train_test_by_id(df_with_id, 0.2, "id")
+
+    print(test_set.head())
